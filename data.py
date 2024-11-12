d1e99e7589688143329232a74ba18d37225f4c66
Ezequiel Germán Palleros Ibarra <ezequiel.palleros@davinci.edu.ar>
2024-11-12 02:07:28 -0300

    create data.py

---
+++ data.py
@@ -0,0 +1,101 @@
+preguntas = {
+    "Cultura general": [
+        ("¿Cuál sería la octava maravilla del mundo?", [
+            ("Las Cataratas del Iguazú", 20000),
+            ("La Gran Muralla China", 10000),
+            ("La Torre Eiffel", 5000),
+            ("El Tah Mahal", 3000),
+            ("El Monumental", 1500),
+            ("La Bombonera", 1000),
+        ]),
+        ("Decime un cuento o película infantil en el que haya un hada.", [
+            ("La Cenicienta", 50000),
+            ("Peter Pan", 25000),
+            ("La Bella Durmiente", 15000),
+            ("Shrek", 10000),
+            ("Pinocho", 5000),
+        ]),
+        ("¿Qué podés hacer con pan?", [
+            ("Sanguche", 100000),
+            ("Tostada", 80000),
+            ("Pan rallado", 60000),
+            ("Budín de pan", 40000),
+        ]),
+    ],
+    "Entretenimiento": [
+        ("¿Qué actor argentino es el más conocido?", [
+            ("Ricardo Darín", 20000),
+            ("Guillermo Francella", 10000),
+            ("Adrián Suar", 5000),
+            ("Leonardo Sbaraglia", 3000),
+            ("Pablo Echarri", 1500),
+            ("Juan Minujín", 1000),
+        ]),
+        ("¿Cuál es la serie de televisión más popular en Argentina?", [
+            ("Los Simuladores", 50000),
+            ("Casados con hijos", 25000),
+            ("El Marginal", 15000),
+            ("Violetta", 10000),
+            ("Graduados", 5000),
+        ]),
+        ("¿Qué cantante argentino es el más famoso?", [
+            ("Charly García", 100000),
+            ("Gustavo Cerati", 80000),
+            ("Luis Alberto Spinetta", 60000),
+            ("Fito Páez", 40000),
+        ]),
+    ],
+    "Deportes": [
+        ("¿Qué deporte NUNCA va a ser parte de los Juegos Olímpicos?", [
+            ("Poker", 20000),
+            ("Piedra, papel o tijera", 10000),
+            ("Quemado", 5000),
+            ("Bowling", 3000),
+            ("Ruleta rusa", 1500),
+            ("Chess-Boxing", 1000),
+        ]),
+        ("Messi podrá ser el mejor futbolista del mundo, pero nunca tendrá ___", [
+            ("Altura/Estatura", 50000),
+            ("Tranquilidad", 25000),
+            ("Facha", 15000),
+            ("Una copa Libertadores", 10000),
+            ("La del Diego", 5000),
+        ]),
+        ("¿Hasta con cuánto tanto del envido cantás la falta?", [
+            ("30", 100000),
+            ("31", 80000),
+            ("27", 60000),
+            ("28", 40000),
+        ]),
+    ],
+    "Historia Argentina": [
+        ("¿Quién es el prócer más importante de Argentina?", [
+            ("José de San Martín", 20000),
+            ("Manuel Belgrano", 10000),
+            ("Juan Manuel de Rosas", 5000),
+            ("Domingo Sarmiento", 3000),
+            ("Juan Domingo Perón", 1500),
+            ("Hipólito Yrigoyen", 1000),
+        ]),
+        ("¿Cuál fue el presidente más importante de Argentina?", [
+            ("Juan Domingo Perón", 50000),
+            ("Raúl Alfonsín", 25000),
+            ("Néstor Kirchner", 15000),
+            ("Julio Argentino Roca", 10000),
+            ("Domingo Faustino Sarmiento", 5000),
+        ]),
+        ("¿Qué personaje histórico creés que tiene un lugar en el billete de 1000?", [
+            ("San Martín", 100000),
+            ("Belgrano", 80000),
+            ("Evita", 60000),
+            ("Artigas", 40000),
+        ]),
+    ]
+}
+
+colores_categoria = {
+    "Cultura general": "lightblue",
+    "Entretenimiento": "lightgreen",
+    "Deportes": "lightcoral",
+    "Historia Argentina": "lightgoldenrod",
+}
